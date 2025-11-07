a0dea44def5f9dc6107a7fce13a584f041d0b28b
Eric Silva <54083779+EricSilvaSt@users.noreply.github.com>
2025-11-07 14:55:32 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Sites from './pages/Sites';
 import Sistemas from './pages/Sistemas';
@@ -9,6 +10,7 @@ import AgentesIA from './pages/AgentesIA';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-white">
         <Header />
         <Routes>
